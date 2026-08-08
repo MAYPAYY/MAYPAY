579d1ac4dbdb6ae9170b82c0ce759468e287d9a9
faisal <faisal@gmail.com>
2026-08-08 12:53:45 +0700

    Tutup ajaUOYY

--- videos.js
+++ videos.js
@@ -10,22 +10,22 @@
 const videoList = [
     {
       id: "https://drive.google.com/file/d/1WXjNMyX0okdCwSfNgMQn3Rgpues-poyr/view?usp=sharing",
-      title: "Video Koleksi 1"
+      title: "Sideo Koleksi 1"
     },
     {
       id: "https://drive.google.com/file/d/1ev2K9TutVe0GZPadUWVgtLmMIqMT0OZj/view?usp=sharing",
-      title: "Video Koleksi 2"
+      title: "Cideo Koleksi 2"
     },
     {
       id: "https://drive.google.com/file/d/1Cg2jbqq8S1PeygzQNq_GMpV4bdaDGhwr/view?usp=sharing",
-      title: "Video Koleksi 3"
+      title: "Sideo Koleksi 3"
     },
     {
       id: "https://drive.google.com/file/d/1iWv-KKInXtOoIsgiayOY3hwkfAHMsBoo/view?usp=sharing",
-      title: "Video Koleksi 4"
+      title: "Cideo Koleksi 4"
     },
     {
       id: "https://drive.google.com/file/d/1_gaVfcl_8HPTnffG0P-yfqFl3XmZO6sv/view?usp=sharing",
-      title: "Video Koleksi 5"
+      title: "Sideo Koleksi 5"
     }
   ];

518769b71f422016926c1231bbeea62bb10d191f
faisal <faisal@gmail.com>
2026-08-08 12:29:43 +0700

    Tambah file videos.js

---
+++ videos.js
@@ -0,0 +1,31 @@
+// ==========================================
+// DAFTAR KOLEKSI VIDEO GOOGLE DRIVE
+// ==========================================
+// Jika ingin menambah video baru, tinggal salin salah satu format di bawah:
+// {
+//   id: "LINK_ATAU_ID_GOOGLE_DRIVE",
+//   title: "JUDUL_VIDEO_ANDA"
+// },
+
+const videoList = [
+    {
+      id: "https://drive.google.com/file/d/1WXjNMyX0okdCwSfNgMQn3Rgpues-poyr/view?usp=sharing",
+      title: "Video Koleksi 1"
+    },
+    {
+      id: "https://drive.google.com/file/d/1ev2K9TutVe0GZPadUWVgtLmMIqMT0OZj/view?usp=sharing",
+      title: "Video Koleksi 2"
+    },
+    {
+      id: "https://drive.google.com/file/d/1Cg2jbqq8S1PeygzQNq_GMpV4bdaDGhwr/view?usp=sharing",
+      title: "Video Koleksi 3"
+    },
+    {
+      id: "https://drive.google.com/file/d/1iWv-KKInXtOoIsgiayOY3hwkfAHMsBoo/view?usp=sharing",
+      title: "Video Koleksi 4"
+    },
+    {
+      id: "https://drive.google.com/file/d/1_gaVfcl_8HPTnffG0P-yfqFl3XmZO6sv/view?usp=sharing",
+      title: "Video Koleksi 5"
+    }
+  ];
